3e31097c75d656baaabfa71735b3ac961d3133ed
Khanh Nguyen <lykhee@outlook.com>
2020-12-24 11:21:48 +1100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
         name='beancount-prices-custom',
         version='1.0',
         packages=['my_sources'],
-        license='MIT',
-        long_description=open('README.md').read(),
+        license='MIT'
 )
