a88cdcb94b584debac8dd298fcaa50a941074e06
Khanh Nguyen <ott.binaural.essay@outlook.com>
2020-12-24 11:11:51 +1100

    Initial commit

---
+++ my_sources/ioof.py
@@ -0,0 +1,72 @@
+
+"""Fetch unit prices from IOOF's website
+"""
+
+from datetime import datetime
+from dateutil import tz
+from dateutil.parser import parse
+import requests
+import json
+from bs4 import BeautifulSoup
+
+from beancount.prices import source
+from beancount.core.number import D
+
+"""
+bean-price -e 'AUD:my_sources.ioof:IOF0097AU'
+"""
+
+BASE_URL = 'https://www.ioof.com.au/performance/unit-prices'
+CURRENCY = 'AUD'
+
+class Source(source.Source):
+
+    def get_url(self, ticker):
+        try:
+            page = requests.get(BASE_URL)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            table = soup.find(class_='IES tertiary hidden fixed')
+            rows = table.find_all('tr')[1:]
+
+            for row in rows:
+                r = {}
+                cols = row.find_all('td')
+                if cols[1].text.strip() == ticker:
+                    url = cols[0].a['href']
+            return url
+        except:
+            raise ValueError('Invalid ticker')
+
+    def get_latest_price(self, ticker):
+        try:
+            url = self.get_url(ticker)
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            table = soup.find(id='unit-price')
+            row = table.find_all('tr')[1]
+
+            col = row.find_all('td')
+            eff_date = parse(col[0].text.strip()).strftime('%Y-%m-%d')
+            time = datetime.strptime(eff_date, '%Y-%m-%d').replace(tzinfo=tz.tzutc())
+            price = D(col[2].text.strip())
+            return source.SourcePrice(price, time, CURRENCY)
+        except UnboundLocalError:
+            return None
+
+    def get_historical_price(self, ticker, time):
+        try:
+            url = self.get_url(ticker)
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            table = soup.find(id='unit-price')
+            rows = table.find_all('tr')[1:]
+
+            date = time.strftime('%Y-%m-%d')
+            for row in rows:
+                cols = row.find_all('td')
+                eff_date = parse(cols[0].text.strip()).strftime('%Y-%m-%d')
+                if eff_date == date:
+                    price = D(cols[2].text.strip())
+            return source.SourcePrice(price, time, CURRENCY)
+        except UnboundLocalError:
+            return None
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name='beancount-prices-custom',
+        version='1.0',
+        packages=['my_sources'],
+        license='MIT',
+        long_description=open('README.md').read(),
+)
